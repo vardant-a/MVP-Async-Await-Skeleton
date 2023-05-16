3afdbdd2957d8b58173438aff840215e0a44039d
Aleksei <lokhin.a.a@gmail.com>
2023-05-16 21:51:57 +0300

    first commit

---
+++ MVP_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP_Project
+//
+//  Created by Aleksei on 16.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
